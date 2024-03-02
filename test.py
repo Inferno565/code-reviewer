6f2db04d256be5a9693362778e333f997851e2d1
Sharif Hamdule <hamdulesharif555@gmail.com>
2024-03-02 11:51:44 +0530

    Implemented function to calculate factorials and display results for user-entered numbers.

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-# def factorial(n):
-#     result = 1
-#     for i in range(1, n + 1):
-#         result *= i
-#     return result
-
-# num = int(input("Enter a number: "))
-
-
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
 
-
-# Get user input
 num = int(input("Enter a number: "))
 
-# Calculate factorial
-result = factorial(num)
 
-print(f"The factorial of {num} is {result}")
+# def factorial(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     return result
+
+
+# # Get user input
+# num = int(input("Enter a number: "))
+
+# # Calculate factorial
+# result = factorial(num)
+
+# print(f"The factorial of {num} is {result}")
